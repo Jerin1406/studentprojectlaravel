93a9fc925a2ec92962afe2c55d35c463b2103550
Jerin K. Thomas <mca519@rcss.com>
2021-03-17 12:46:19 +0530

    heroku laravel

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -5,15 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student</title>
 </head>
 <body>
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/">Student</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/contact">Contact</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/faculty">Faculty</a>
+  </li>
+ 
+</ul>
 <div class="container">
     <div class="row">
-        <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="col col-12 col-sm-3 col-md-3 col-lg-3">
         
         </div>
-        <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
             <table class="table table-borderless ">
                 <tr>
                     <td>Username</td>
@@ -39,6 +51,6 @@
         </div>
     </div>
 </div>
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('student');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/faculty', function () {
+    return view('faculty');
 });
